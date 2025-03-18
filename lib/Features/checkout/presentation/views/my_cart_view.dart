b4ac5e7347a6e1ef04049a2205d550d93ec02544
ahmed shenhab <ahmedshenhab123321@gmail.com>
2025-03-18 19:16:42 +0200

    no important  changes

--- lib/Features/checkout/presentation/views/my_cart_view.dart
+++ lib/Features/checkout/presentation/views/my_cart_view.dart
@@ -9,7 +9,7 @@ class MyCartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(title: 'My Car'),
+      appBar: buildAppBar(title: 'My Cart'),
       body: const MyCartViewBody(),
     );
   }
